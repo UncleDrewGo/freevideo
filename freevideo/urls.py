6f726ec0116c59fd5215ed67fa636be7837eda17
wj <wj@umscloud.com>
2018-01-16 21:52:08 +0800

    update url

--- freevideo/urls.py
+++ freevideo/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from videoapp.views import firstView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('people/', firstView)
+    path('', firstView),
 ]
